314ea24ab04ff97d5a1e5aef59bc1d6fedaadca7
Seckin Alp Kargi <seckinalpkargi@gmail.com>
2024-06-15 16:07:33 +0300

    preferences

--- App/buy_best/users/models.py
+++ App/buy_best/users/models.py
@@ -1,7 +1,7 @@
 # myapp/models.py
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-
+from django.conf import settings
 class CustomUser(AbstractUser):
     groups = models.ManyToManyField(
         Group,
@@ -17,3 +17,14 @@ class CustomUser(AbstractUser):
         help_text='Specific permissions for this user.',
         related_query_name='user',
     )
+
+class UserPreference(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    price_min = models.IntegerField(null=True, blank=True)
+    price_max = models.IntegerField(null=True, blank=True)
+    product_type = models.CharField(max_length=100, null=True, blank=True)
+    brand = models.CharField(max_length=100, null=True, blank=True)
+    keywords = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Preferences"
